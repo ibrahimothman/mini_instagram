68e770143f00867da0d4dbfaa7fc654d7ad6832d
ibrahimothman <ibrahimothmanre@gmail.com>
2019-10-20 14:37:34 +0200

    add open sans font

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuBRzld_UHHLJcGlXadCWnv7IkdZAbBDWIdBG2nPrRVTzgC72P' class="rounded-circle w-100">
+            </div>
+            <div class="col-9 pt-5">
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <div class="d-flex align-items-center pb-3">
+                        <div class="h4">ibra karim</div>
+                    </div>
+
+                </div>
+
+
+
+                <div class="d-flex">
+                    <div class="pr-5"><strong>10</strong> posts</div>
+                    <div class="pr-5"><strong>10</strong> followers</div>
+                    <div class="pr-5"><strong>20</strong> following</div>
+                </div>
+                <div class="pt-4 font-weight-bold">ibra</div>
+                <div>Description</div>
+                <div><a href="#">ibra.com</a></div>
+            </div>
+        </div>
+
+        <div class="row pt-5">
+        </div>
+    </div>
+@endsection
