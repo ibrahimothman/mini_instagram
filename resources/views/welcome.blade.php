df42da68f2110a8274531512a409a1610d46b2ff
ibrahimothman <ibrahimothmanre@gmail.com>
2019-10-20 14:15:20 +0200

    Add auth feat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    mini instagram
                 </div>
 
                 <div class="links">
